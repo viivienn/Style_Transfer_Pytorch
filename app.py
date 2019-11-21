5e4d7d63cf7d05b8c7d047c22cfc852ff3f573e9
Vivien Chow <vivien-yu.chow.1@ens.etsmtl.ca>
2019-11-20 19:37:07 -0500

    app change

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def upload_file():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+    app.run(threaded=True, port=int(os.environ.get('PORT', 5000)))
